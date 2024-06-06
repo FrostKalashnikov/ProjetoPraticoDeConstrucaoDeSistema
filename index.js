6c206d48c9193ee5fcd9ce9e341c0fe6aec7399f
Frost <michelbrito2001@gmail.com>
2024-06-06 11:08:18 -0300

    servidor node.js com express

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-(async ()=>{
-    const db=require('./db')
-})()
+const express = require('express')
+const path = require('path')
+const app = express()
+const porta = process.env.PORT 
+
+(async ()=>{ const db = require('./src/db') })()
+
+app.use(express.static(path.join(__dirname, 'html')))
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'html', '/index.html'))
+
+})
+
+
+app.listen(porta || 3000, ()=>{console.log('servidor rodando')})
 
